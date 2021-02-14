718aca6e1aa9e582784f71a518fa0df5de56bb78
mohamedisakr <mohamedisakr@gmail.com>
2021-02-14 13:14:35 +0200

    implementing reverse array function

---
+++ objects-arrays/array-reverse.js
@@ -0,0 +1,24 @@
+/*
+Reversing an array
+Arrays have a method reverse, which changes the array by inverting the
+order in which its elements appear. For this exercise, write two functions,
+reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array
+as argument and produces a new array that has the same elements in
+the inverse order. The second, reverseArrayInPlace, does what the reverse
+method does: it modifies the array given as argument in order to reverse
+its elements. Neither may use the standard reverse method.
+Thinking back to the notes about side effects and pure functions in
+the previous chapter, which variant do you expect to be useful in more
+situations? Which one is more efficient?
+*/
+function reverseArray(array) {
+  let result = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    result.push(array[i]);
+  }
+  return result;
+}
+
+const array = [1, 3, 5, 7, 9];
+const result = reverseArray(array);
+console.log(result);
